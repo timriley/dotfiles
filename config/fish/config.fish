528e7bab4eea526c77adfd07d84d2964d67905ad
Tim Riley <tim@openmonkey.com>
2016-08-09 08:30:50 +1000

    Improve fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,17 @@
+# Turn off the default greeting
+set --erase fish_greeting
+
 status --is-interactive; and . (rbenv init -|psub)
 status --is-interactive; and . (nodenv init -|psub)
 
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showcolorhints 'yes'
+
 set --export EDITOR "subl"
+
+set --export HOMEBREW_CASK_OPTS "--appdir=/Applications"
+
+function fish_user_key_bindings
+  # Thanks, https://github.com/fish-shell/fish-shell/issues/905#issuecomment-20559486
+  bind \cc 'echo; commandline ""; commandline -f repaint'
+end
