d20f3ad3f2b71963c38eb4cf4fada8002c015e8b
Tim Riley <tim@openmonkey.com>
2019-06-04 14:58:16 +1000

    Make some major workaround for Fish's path setup

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,8 +1,19 @@
 # Turn off the default greeting
 set fish_greeting
 
-# Add Postgres.app's bin to path, but before asdf inits, so asdf's shims end up first
-set PATH /Applications/Postgres.app/Contents/Versions/latest/bin $PATH
+# Fish's login shell PATH setup is infuriatingly broken with version 3.0.x, with
+# e.g. /usr/local/bin being put _after_ /usr/bin.
+#
+# Specify my own "preferred" path dirs here (in reverse order) and ensure they
+# get re-placed at the front of the current $PATH.
+#
+# Postgres.app's bin dir is added here, specifically before asdf inits, so #
+# asdf's shims end up first of all (allowing Postgres.app to be seen by asdf as
+# the "system" postgres)
+set -l extra_path_dirs /usr/local/sbin /usr/local/bin /Applications/Postgres.app/Contents/Versions/latest/bin
+for extra_dir in $extra_path_dirs
+  set PATH $extra_dir (string match -v $extra_dir $PATH)
+end
 
 status --is-interactive; and source /usr/local/opt/asdf/asdf.fish
 
