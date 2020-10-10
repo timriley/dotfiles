e590a41becbfb8560104bcba0201fac1eec0b403
Tim Riley <tim@openmonkey.com>
2020-10-10 15:13:14 +1100

    Change iTerm color scheme on launch

--- Library/Application Support/iTerm2/Scripts/AutoLaunch/change_color_preset_on_theme_change.py
+++ Library/Application Support/iTerm2/Scripts/AutoLaunch/change_color_preset_on_theme_change.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.7
+#!/usr/bin/env python3
 
 # This script was created with the "basic" environment which does not support
 # adding dependencies with pip.
@@ -8,24 +8,28 @@
 import asyncio
 import iterm2
 
+async def update(connection, theme):
+    # Themes have space-delimited attributes, one of which will be light or dark.
+    parts = theme.split(" ")
+    if "dark" in parts:
+        preset = await iterm2.ColorPreset.async_get(connection, "base16-summerfruit-dark-256")
+    else:
+        preset = await iterm2.ColorPreset.async_get(connection, "base16-summerfruit-light-256")
+
+    # Update the list of all profiles and iterate over them.
+    profiles=await iterm2.PartialProfile.async_query(connection)
+    for partial in profiles:
+        # Fetch the full profile and then set the color preset in it.
+        profile = await partial.async_get_full_profile()
+        await profile.async_set_color_preset(preset)
+
 async def main(connection):
+    app = await iterm2.async_get_app(connection)
+    await update(connection, await app.async_get_variable("effectiveTheme"))
     async with iterm2.VariableMonitor(connection, iterm2.VariableScopes.APP, "effectiveTheme", None) as mon:
         while True:
             # Block until theme changes
             theme = await mon.async_get()
-
-            # Themes have space-delimited attributes, one of which will be light or dark.
-            parts = theme.split(" ")
-            if "dark" in parts:
-                preset = await iterm2.ColorPreset.async_get(connection, "base16-summerfruit-dark-256")
-            else:
-                preset = await iterm2.ColorPreset.async_get(connection, "base16-summerfruit-light-256")
-
-            # Update the list of all profiles and iterate over them.
-            profiles=await iterm2.PartialProfile.async_query(connection)
-            for partial in profiles:
-                # Fetch the full profile and then set the color preset in it.
-                profile = await partial.async_get_full_profile()
-                await profile.async_set_color_preset(preset)
+            await update(connection, theme)
 
 iterm2.run_forever(main)
