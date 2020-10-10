b2d8eb87e717403d88e36acd164e657197b3f0c9
Tim Riley <tim@openmonkey.com>
2020-10-10 15:12:43 +1100

    Fix ghr function name

--- config/fish/functions/ghr.fish
+++ config/fish/functions/ghr.fish
@@ -6,7 +6,7 @@ if not set -q GH_BASE_DIR
     set GH_BASE_DIR $HOME/src
 end
 
-function gh -d "manage git repos"
+function ghr -d "manage git repos"
   set git_host github.com
   set -l repo ""
 
@@ -15,7 +15,7 @@ function gh -d "manage git repos"
   else if [ (count $argv) -eq 2 ]
     set repo $argv[1]/$argv[2]
   else
-    echo "USAGE: gh [user] [repo]"
+    echo "USAGE: ghr [user] [repo]"
     return -1
   end
 
