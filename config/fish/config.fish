072cdf214bcbd3e1ce89e78b242b0eed5b43d94b
Tim Riley <tim@riley.id.au>
2022-06-02 09:58:49 +1000

    Update for new homebrew install path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -11,13 +11,18 @@ for extra_dir in $extra_path_dirs
   set PATH $extra_dir (string match -v $extra_dir $PATH)
 end
 
+fish_add_path /opt/homebrew/bin
+
 direnv hook fish | source
 
-source /usr/local/opt/asdf/asdf.fish
+source /opt/homebrew/opt/asdf/asdf.fish
 
 source ~/.secrets.fish
 
 set --export EDITOR "code"
+
+# Required for postgres to build, see https://github.com/smashedtoatoms/asdf-postgres/issues/52
+set --export HOMEBREW_PREFIX "/opt/homebrew"
 
 set --export HOMEBREW_CASK_OPTS "--appdir=/Applications"
 set --export HOMEBREW_NO_AUTO_UPDATE "1"
