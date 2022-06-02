1d4a402da234119420c7a6f569613cd8d4c4a0e5
Tim Riley <tim@riley.id.au>
2022-06-02 10:04:28 +1000

    Remove hack for inserting paths

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,16 +1,6 @@
 # Turn off the default greeting
 set fish_greeting
 
-# Specify my preferred path dirs here (in reverse order) and ensure they get
-# re-placed at the front of the current $PATH.
-#
-# Include Postgres.app here, before asdf inits, so asdf's shims end up first
-# of all (allowing Postgres.app to be seen by asdf as the "system" postgres)
-set -l extra_path_dirs /Applications/Postgres.app/Contents/Versions/latest/bin
-for extra_dir in $extra_path_dirs
-  set PATH $extra_dir (string match -v $extra_dir $PATH)
-end
-
 fish_add_path /opt/homebrew/bin
 
 direnv hook fish | source
